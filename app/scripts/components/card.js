e683d9d85d22b8824ddd4b365ad416a2fcbc5b81
wilsonwen <wilson.wen.chn@gmail.com>
2017-07-06 23:24:10 +0800

    change file name capital letter

--- app/scripts/components/card.js
+++ app/scripts/components/card.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Video from './Video';
+import Video from './video';
 import ReactPlayer from 'react-player'
 let numeral = require('numeral');
 let backdropIMG;
